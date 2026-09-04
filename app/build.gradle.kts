8ac9eb74a79068484e3417daf84dfc46d1ba13e5
ahmedwajeehk-crypto <ahmedwajeehk@gmail.com>
2026-09-04 20:57:05 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.ankidroidbridge"
+        applicationId = "com.example.ankidroidbridge2"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
