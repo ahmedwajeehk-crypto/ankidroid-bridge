c3442394f4cc45ae571b7aeb7a008b89256a703d
ahmedwajeehk-crypto <ahmedwajeehk@gmail.com>
2026-09-04 20:33:05 +0500

    Update AnkiBridgeReceiver.java

--- app/src/main/java/com/example/ankidroidbridge/AnkiBridgeReceiver.java
+++ app/src/main/java/com/example/ankidroidbridge/AnkiBridgeReceiver.java
@@ -3,17 +3,23 @@ package com.example.ankidroidbridge;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.util.Log;
 
 public class AnkiBridgeReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
         String taxonomyId = intent.getStringExtra("taxonomy_id");
 
-        Toast.makeText(
-                context,
-                "Bridge received taxonomy_id: " + taxonomyId,
-                Toast.LENGTH_LONG
-        ).show();
+        Log.e(
+            "ANKI_BRIDGE",
+            "RECEIVED taxonomy_id=" + taxonomyId
+        );
+
+        Intent reply = new Intent("com.example.ankidroidbridge.REPLY");
+        reply.setPackage("com.arlosoft.macrodroid");
+        reply.putExtra("taxonomy_id", taxonomyId);
+        context.sendBroadcast(reply);
     }
 }
