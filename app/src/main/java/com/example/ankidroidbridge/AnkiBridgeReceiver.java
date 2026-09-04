82342d5671717fa32ed8ad46e61e96663f9968ea
ahmedwajeehk-crypto <ahmedwajeehk@gmail.com>
2026-09-04 21:07:47 +0500

    Update AnkiBridgeReceiver.java

--- app/src/main/java/com/example/ankidroidbridge/AnkiBridgeReceiver.java
+++ app/src/main/java/com/example/ankidroidbridge/AnkiBridgeReceiver.java
@@ -3,7 +3,10 @@ package com.example.ankidroidbridge;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
 
 public class AnkiBridgeReceiver extends BroadcastReceiver {
 
@@ -12,14 +15,38 @@ public class AnkiBridgeReceiver extends BroadcastReceiver {
 
         String taxonomyId = intent.getStringExtra("taxonomy_id");
 
-        Log.e(
-            "ANKI_BRIDGE",
-            "RECEIVED taxonomy_id=" + taxonomyId
-        );
+        try {
+            Uri modelsUri =
+                    Uri.parse("content://com.ichi2.anki.flashcards/models");
 
-        Intent reply = new Intent("com.example.ankidroidbridge.REPLY");
-        reply.setPackage("com.arlosoft.macrodroid");
-        reply.putExtra("taxonomy_id", taxonomyId);
-        context.sendBroadcast(reply);
+            Cursor cursor = context.getContentResolver().query(
+                    modelsUri,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+
+            int count = 0;
+
+            if (cursor != null) {
+                count = cursor.getCount();
+                cursor.close();
+            }
+
+            String message =
+                    "Anki access SUCCESS. Models found: " + count
+                    + " | taxonomy_id=" + taxonomyId;
+
+            Log.e("ANKI_BRIDGE", message);
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+
+            String message = "ANKI ERROR: " + e.toString();
+
+            Log.e("ANKI_BRIDGE", message);
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
     }
 }
